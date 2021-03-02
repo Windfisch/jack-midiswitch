fca4f4e4cf5067368ea86439c72ce218696be400
Florian Jung <flo@windfis.ch>
2021-03-02 01:02:29 +0100

    Create jack client and copy midi events.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,82 @@
 use jack;
 use clap;
 
+/*struct Notifications;
+impl jack::NotificationHandler for Notifications {
+
+}*/
+
 fn main() {
 	let args = clap::App::new("jack-midiswitch")
 		.version("0.1.0")
 		.author("Florian Jung <flo@windfis.ch>")
 		.about("A keyboard controlled jack midi port multiplexer")
-		.arg(clap::Arg::with_name("input")
-			.short("i")
-			.long("input")
+		.arg(clap::Arg::with_name("split")
+			.short("s")
+			.long("split")
 			.takes_value(true)
 			.multiple(true)
 			.help("Input key mapping(s). Can be one or multiple strings, e.g. 'qwerty' and 'asdf'. This would create one input port that can be forwarded to six different output ports (selectable by the qwerty keys), and another input port that can be forwarded to four output ports.")
 		)
-		.arg(clap::Arg::with_name("output")
-			.short("o")
-			.long("output")
+		.arg(clap::Arg::with_name("merge")
+			.short("m")
+			.long("merge")
 			.takes_value(true)
 			.multiple(true)
 			.help("Output key mapping(s). Same syntax as above, but creates N input ports that are mapped to one output ports.")
 		)
+		.arg(clap::Arg::with_name("client_name")
+			.short("n")
+			.long("client_name")
+			.help("The jack client name (default: 'midiswitch')")
+		)
 		.get_matches();
 
-	let input_mappings = args.values_of_lossy("input").unwrap_or(vec![]);
-	let output_mappings = args.values_of_lossy("output").unwrap_or(vec![]);
+	let split_mappings: Vec<String> = args.values_of_lossy("split").unwrap_or(vec![]).into_iter().filter(|s| !s.is_empty()).collect();
+	let merge_mappings: Vec<String> = args.values_of_lossy("merge").unwrap_or(vec![]).into_iter().filter(|s| !s.is_empty()).collect();
+	let client_name: String = args.value_of_lossy("client_name").map(|s| s.into()).unwrap_or("midiswitch".into());
 
-	println!("{:?}", input_mappings);
+	let client = jack::Client::new(&client_name, jack::ClientOptions::NO_START_SERVER).expect("Failed to connect to JACK").0;
+	
+	let mut split_ports: Vec<_> = split_mappings.iter().map(|mapping| {
+		(
+			0 as usize,
+			client.register_port(&format!("{}_in", mapping), jack::MidiIn).expect(&format!("Failed to register input port {}_in", mapping)),
+			mapping.chars().map(|character| {
+				client.register_port(&format!("{}_out_{}", mapping, character), jack::MidiOut).expect(&format!("Failed to register output port {}_out_{}", mapping, character))
+			}).collect::<Vec<_>>()
+		)
+	}).collect();
+
+	let mut merge_ports: Vec<_> = merge_mappings.iter().map(|mapping| {
+		(
+			0 as usize,
+			client.register_port(&format!("{}_out", mapping), jack::MidiOut).expect(&format!("Failed to register output port {}_out", mapping)),
+			mapping.chars().map(|character| {
+				client.register_port(&format!("{}_in_{}", mapping, character), jack::MidiIn).expect(&format!("Failed to register input port {}_in_{}", mapping, character))
+			}).collect::<Vec<_>>()
+		)
+	}).collect();
+
+	let async_client = client.activate_async((), jack::ClosureProcessHandler::new(move |client: &jack::Client, scope: &jack::ProcessScope| -> jack::Control {
+		
+		// copy events from activated in->out port connections
+		for (out_idx, in_port, out_ports) in split_ports.iter_mut() {
+			let mut writer = out_ports[*out_idx].writer(scope);
+			for event in in_port.iter(scope) {
+				writer.write(&event).ok();
+			}
+		}
+		for (in_idx, out_port, in_ports) in merge_ports.iter_mut() {
+			let mut writer = out_port.writer(scope);
+			for event in in_ports[*in_idx].iter(scope) {
+				writer.write(&event).ok();
+			}
+		}
+		return jack::Control::Continue;
+	})).expect("Failed to activate client");
+
+	std::thread::sleep(std::time::Duration::from_secs(10));
 
     println!("Hello, world!");
 }
